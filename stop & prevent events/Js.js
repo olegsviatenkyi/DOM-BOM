31ae90c0ff277e3776d0099029dd1ac3a3a2e6d6
github.com/olegsviatenkyi <oleg160183@gmail.com>
2018-01-10 15:47:09 +0200

    Added new folder 'stop & prevent' (index.html, style.css, Js.js)

---
+++ stop & prevent events/Js.js
@@ -0,0 +1,30 @@
+
+var onClick = function (e) {
+	var $el = e.target;
+
+
+  $el.style.backgroundColor = 'yellow';
+	alert('CLICK!');
+   setTimeout(function(){
+    $el.style.backgroundColor = '';
+	}, 500)
+}
+
+document.body.onclick = onClick;
+box1.onclick = onClick;
+box2.onclick = onClick;
+box3.onclick = onClick;
+
+var stop = function(e){
+		e.stopPropagation();
+		alert("stopPropagation() зупиняє спливання події по DOM до батьківських елементів")
+	}
+	var prevent = function(e){
+		e.preventDefault (e);
+		alert("preventDefault() зупинить обробку події браузером за замовчуванням")
+	}
+
+	var classStop = document.querySelector('.stop');
+	var classPrevent = document.querySelector('.prevent');
+	classStop.addEventListener("click", stop);
+	classPrevent.addEventListener("click", prevent);
