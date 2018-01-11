440e9554515fe8dcd4bf3836db696d4afc8b4f00
github.com/olegsviatenkyi <oleg160183@gmail.com>
2018-01-11 14:30:33 +0200

    Added folder 'localization' (index.html, style.css, Js.js)

---
+++ localization/Js.js
@@ -0,0 +1,37 @@
+// var userName = 
+
+function loadName(){
+	var userName = localStorage.getItem('userName') || prompt("Your name") || "username";
+	var myName = document.querySelector('.username');
+	myName.innerHTML = userName;
+}
+function loadLanguage() {
+	var defaultLanguage = "ua";
+  var language = localStorage.getItem('language') || defaultLanguage;
+
+  var $radio = document.querySelector('#languages input[data-language="' + language + '"]');
+  $radio.checked = true;
+  updateI18nTexts();
+}
+function saveName(userName){
+	localStorage.setItem('userName', userName);
+}
+function saveLanguage(language) {
+  localStorage.setItem('language', language);
+  updateI18nTexts();
+}
+
+function updateI18nTexts() {
+	var language = localStorage.getItem('language');
+  document.body.setAttribute('data-current-language', language);
+}
+
+document.querySelector('#save').addEventListener('click', function (e) {
+  e.preventDefault();
+  var language = document.querySelector("#languages input:checked").getAttribute('data-language');
+  var userName = document.querySelector(".username").innerHTML;
+  saveLanguage(language);
+  saveName(userName);
+});
+loadName();
+loadLanguage();
