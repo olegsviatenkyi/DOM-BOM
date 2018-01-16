6bbc7ea8f8851f23d74dbb81f09f00c74b7f53c5
github.com/olegsviatenkyi <oleg160183@gmail.com>
2018-01-16 12:53:50 +0200

    modified tasks 'list of users' and 'player navigation'

--- list of users/js/app.js
+++ list of users/js/app.js
@@ -6,12 +6,47 @@ function addEmployee() {
 	var limit = document.querySelector("#limit").value;
 	var limitAvgSalary = document.querySelector("#limitAvgSalary").value;
 	var button = document.querySelector("#addEmployee");
-	var limitText = "the limit of the number of employees was reached";
-	var limitAvgSalaryText = "the limit of the avg. salary was reached";
+	var LIMIT_TEXT = "the limit of the number of employees was reached";
+	var LIMIT_AVG_SALARY_TEXT = "the limit of the avg. salary was reached";
 	var number = document.querySelector(".number");
 	var resultSalary = document.querySelector(".salary");
 	
-	function addUlLiSpan(){
+	function addCheckEmployee(){
+		function checkingFirstName(){
+			var first = document.getElementsByClassName("employeeFirstName");
+			var arrFirstName = new Array();
+			for(var i=0; i<first.length; i++) {
+				var firstText = first[i].innerHTML;
+				arrFirstName.push(firstText);
+			}
+			var idx = arrFirstName.indexOf(firstName + " / ");
+			return idx;	
+		}
+
+		function checkingLastName(){
+			var last = document.getElementsByClassName("employeeLastName");
+			var arrLastName = new Array();
+			for(var i=0; i<last.length; i++) {
+				var lastText = last[i].innerHTML;
+				arrLastName.push(lastText);
+			}
+			var idx = arrLastName.indexOf(lastName + " / ");
+			return idx;	
+		}
+
+		function avgSalary(){
+			var salaries = document.getElementsByClassName("employeeSalary");
+			var arrSalary = new Array();
+			for(var i=0; i<salaries.length; i++) {
+				var salariesText = salaries[i].innerHTML;
+				arrSalary.push(parseInt(salariesText));
+			}
+			var avg = arrSalary.reduce(function(previousValue, currentValue) {
+  			return previousValue + currentValue;
+			},0);
+			var avgSalary = avg / salaries.length;
+			return (avgSalary.toFixed(0))
+		}
 		var newUl = document.querySelector('.employeeList');
 		var newLi = document.createElement('li');
 		var newSpan1 = document.createElement('span');
@@ -28,69 +63,28 @@ function addEmployee() {
 			newSpan3.classList.add('employeeSalary');
 			if (salary > 0){
 				newSpan3.innerHTML = salary;
-			
-
 			var newSpan4 = document.createElement('span');
 			newSpan4.classList.add('employeePosition');
 			if (position.match(/[a-zA-Z]/)) {
 			newSpan4.innerHTML = "$ / " + position ;
-
 			var employees = document.getElementsByTagName('li');
 			var employeesNumber = employees.length;
-			if(employeesNumber < limit){
-
-				function checkingFirstName(){
-					var first = document.getElementsByClassName("employeeFirstName");
-					var arrFirstName = new Array();
-					for(var i=0; i<first.length; i++) {
-						var firstText = first[i].innerHTML;
-						arrFirstName.push(firstText);
-					}
-					var idx = arrFirstName.indexOf(firstName + " / ");
-					return idx;	
-				}
-
-				function checkingLastName(){
-					var last = document.getElementsByClassName("employeeLastName");
-					var arrLastName = new Array();
-					for(var i=0; i<last.length; i++) {
-						var lastText = last[i].innerHTML;
-						arrLastName.push(lastText);
-					}
-					var idx = arrLastName.indexOf(lastName + " / ");
-					return idx;	
-				}
-
-				function avgSalary(){
-					var salaries = document.getElementsByClassName("employeeSalary");
-					var arrSalary = new Array();
-					for(var i=0; i<salaries.length; i++) {
-						var salariesText = salaries[i].innerHTML;
-						arrSalary.push(parseInt(salariesText));
-					}
-					var avg = arrSalary.reduce(function(previousValue, currentValue) {
-  					return previousValue + currentValue;
-					},0);
-				 var avgSalary = avg / salaries.length;
-				 return (avgSalary.toFixed(0))
-				}
+			if(employeesNumber < limit){				
+				if((checkingFirstName() === -1 && checkingLastName() === -1) || (checkingFirstName() === -1 && checkingLastName() > -1) || (checkingFirstName() > -1 && checkingLastName() === -1)){
 				
-				if(checkingFirstName() === -1){
-				if(checkingLastName() === -1){
 					newUl.appendChild(newLi);
 					newLi.appendChild(newSpan1);
 					newLi.appendChild(newSpan2);
 					newLi.appendChild(newSpan3);
 					newLi.appendChild(newSpan4);
-			}else alert("An employee with that last name already exists")		
-			}else alert("An employee with that first name already exists")
-
+			}else alert("An employee with that first name and last name already exists")		
+			
 			if (avgSalary () > limitAvgSalary){
-                newUl.removeChild(newUl.lastChild);
-                alert(limitAvgSalaryText);
+        newUl.removeChild(newUl.lastChild);
+        alert(LIMIT_AVG_SALARY_TEXT);
       }
 
-			}else {alert(limitText)}
+			}else {alert(LIMIT_TEXT)}
 			
 				employeesNumber = employees.length;
 				number.innerHTML = "Employees: " + employeesNumber;
@@ -102,5 +96,5 @@ function addEmployee() {
 
 	}
 
-	addUlLiSpan();
+	addCheckEmployee();
 }
--- player navigation/Js.js
+++ player navigation/Js.js
@@ -11,6 +11,15 @@ document.body.onkeydown = function (e) {
   var KEYCODE_DOWN = 40;
   var KEYCODE_SPACE = 32;
   var KEYCODE_ENTER = 13; 
+  var sizeFire = 10;
+  function fire1(){
+    sizeFire = sizeFire + 10;
+    image.style.width = sizeFire + 'px'; 
+    if (sizeFire < 400) {
+      setTimeout(fire1, 100); 
+    }
+      else el.removeChild(image);
+    }
   
   if (e.keyCode == KEYCODE_LEFT) {
     el.style.left = (parseInt(el.style.left) - 10) + 'px';
@@ -28,35 +37,13 @@ document.body.onkeydown = function (e) {
     var image = document.createElement("img");
     image.setAttribute("src", "fire.jpg");
     el.appendChild(image); 
-    var sizeFire = 10;
-    function fire1(){
-      sizeFire = sizeFire + 10;
-      image.style.width = sizeFire + 'px'; 
-      if (sizeFire < 400) {
-      
-      setTimeout(fire1, 100); 
-      }
-       else el.removeChild(image);
-
-    }
     fire1(); 
   }
   else if (e.keyCode == KEYCODE_ENTER) {
     var image = document.createElement("img");
     image.setAttribute("src", "fire1.jpg");
     el.appendChild(image); 
-    var sizeFire = 10;
-    function fire1(){
-      sizeFire = sizeFire + 10;
-      image.style.width = sizeFire + 'px'; 
-      if (sizeFire < 400) {
-      
-      setTimeout(fire1, 100); 
-      }
-      else el.removeChild(image);
-    }
     fire1(); 
-
   }
   
 }
